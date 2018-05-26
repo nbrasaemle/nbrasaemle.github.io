4b8cde0165655260aa950e50ad59d365b4b7caca
nbrasaemle <nicole.brasaemle@gmail.com>
2018-05-26 17:54:27 -0500

    psychic done

--- hw3/Psychic-Game/assets/javascript/game.js
+++ hw3/Psychic-Game/assets/javascript/game.js
@@ -12,56 +12,62 @@
 //if run out of guesses, add to the number of losses
 
 //loop to play another round
-$(document).ready(function() {
 
+
+$(document).ready(function() { 
     var wins = 0;
     var losses = 0;
     var maxGuesses = 10;
     var userGuesses = [];
     var userGuess;
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "x", "y", "z"];
 
-    //pick a random letter
-    function computerLetterFunction() {
-        var letters = ["a", "b", "c", "d", "e", "f", "g", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "x", "y", "z"];
-        var randomLetter = letters[Math.floor(Math.random() * letters.length)];
-        console.log(randomLetter);
-        return randomLetter;
+    function initializeGame() {
+        //this is so the game can restart without refreshing the page
+        maxGuesses=10;
+        userGuesses=[];
+        compLetter = computerLetterFunction();
+
     }
 
-    var compLetter = computerLetterFunction();
-        console.log(computerLetterFunction);
-    document.onkeyup = function(event){
-        console.log(event);
-    
+    //pick a random letter
+    function computerLetterFunction() {  
+        var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+        console.log('random letter is: ', randomLetter);
+        return randomLetter; //store the letter, stop the function
+    }
+
+    var compLetter = computerLetterFunction(); //storing the result of the function into a variable
+        console.log('compLetter is: ', compLetter);
+    //starting the function to start the game with the press of a key
+    document.onkeyup= function checkLetter(event){
         
-        userGuess=event.key;
-        
+        userGuess=event.key; //puts the used key into a variable
+        console.log(userGuess)
+      
         if (userGuess == compLetter) {
             wins++;
-            maxGuesses=10;
-            
+            document.getElementById("answer").innerHTML = "The answer was: " + compLetter;
+            initializeGame();
         }
         else{
             maxGuesses--;
-            userGuesses.push(userGuess);
+            userGuesses.push(userGuess); //putting the incorrect guess into an array
+           
         }   
         if (maxGuesses == 0) {
             losses++;
-            maxGuesses=10;
-            delete userGuesses[0];
+            document.getElementById("answer").innerHTML = "The answer was: " + compLetter;
+            
             //reset ther user guess and clear the array to start over?
-            var compLetter = computerLetterFunction();
+            initializeGame();
         }
+        //print in html
         document.getElementById("wins").innerHTML = "Wins: " + wins;
         document.getElementById("losses").innerHTML = "Losses: " + losses;
         document.getElementById("guesses").innerHTML = "Guesses Left: " + maxGuesses;
         document.getElementById("userGuesses").innerHTML = "Letters that have been guessed already: " + userGuesses;
-
+        
     }
-
-
-
-
-
 
 });
