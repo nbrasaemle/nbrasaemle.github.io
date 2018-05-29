f21e08c515fab08d80bb07b112b8196e24d155da
nbrasaemle <nicole.brasaemle@gmail.com>
2018-05-29 17:25:16 -0500

    word game update

--- hw3/Word-Guess-Game/assets/javascript/game.js
+++ hw3/Word-Guess-Game/assets/javascript/game.js
@@ -1,1 +1,30 @@
 console.log("Testing");
+
+$(document).ready(function(){
+
+
+//variables
+var wins = 0;
+var losses =0;
+var maxGuesses=10;
+var guessedLetters=[];
+
+//Object of words to guess from
+var lotrNames= ["aragorn", "legolas", "gandalf"]
+
+//function
+
+
+//for loop
+
+
+//letters only
+
+//blanks
+
+//1 letter at a time
+
+
+
+
+});
