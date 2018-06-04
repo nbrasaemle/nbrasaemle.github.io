90b1d988715353054780689096b343dbfe748a22
nbrasaemle <nicole.brasaemle@gmail.com>
2018-06-03 23:30:44 -0500

    updates with some bugs

--- unit-4-game1/assets/javascript/game.js
+++ unit-4-game1/assets/javascript/game.js
@@ -17,23 +17,24 @@ $(document).ready(function(){
         //have to either have the number above the max or add one to get the range desired
         var valueGandalf = Math.floor(Math.random() * (13-1) + 1);
         console.log("Gandalf's value: " + valueGandalf);
-        var valueHobbits = Math.floor(Math.random() * (13-1) + 1);
+        var valueHobbits = Math.floor(Math.random() * (13-1) + 1); // hobbits value is coming up as gandalfs value
         console.log("Hobbits' value: " + valueHobbits);
         var valueAragorn = Math.floor(Math.random() * (13-1) + 1);
         console.log("Aragorn's value: " + valueAragorn);
-        var valueBoromir = Math.floor(Math.random() * (13-1) + 1);
+        var valueBoromir = Math.floor(Math.random() * (13-1) + 1);//come up as gandalf
         console.log("Boromir's value: " + valueBoromir);
         
         //give the buttons these values.val() or .attr()
-        $(".gandalf").attr(valueGandalf);
-        $(".hobbits").attr(valueHobbits);
-        $(".aragorn").attr(valueAragorn);
-        $(".boromir").attr(valueBoromir);
+        $("button.gandalf").attr("data-value", valueGandalf);
+        $("button.hobbits").attr("data-value", valueHobbits);
+        $("button.aragorn").attr("data-value", valueAragorn);
+        $("button.boromir").attr("data-value", valueBoromir);
         //print the random number 
         $(".randomNumber").html("<h2> " + randomNumber + " </h2>");
         //print the total score
-        $(".totalScore").html("<h2>Total Score: </h2>");
-
+        $(".totalScore").html("<h2>Total Score: </h2> <h2> " + totalScore + " </h2>");
+        $("#wins").html("Wins: ");
+        $("#losses").html("Losses: ");
     }
 
     resetGame();
@@ -45,7 +46,7 @@ $(document).ready(function(){
     function winLose() {
         if (totalScore == randomNumber) {
             wins++;
-            $("#win_lose").html("You Win! The Fellowship has been reunited!")
+            $("#win_lose").html("The Fellowship has been reunited!")
             $("#wins").html("Wins: " + wins );
     
             resetGame();
@@ -53,18 +54,23 @@ $(document).ready(function(){
     
         else if (totalScore > randomNumber) {
             losses++;
-            $("#win_lose").html("You Lose! The Fellowship is lost!")
+            $("#win_lose").html("The Fellowship is lost!")
             $("#losses").html("Losses: " + losses);
     
             resetGame();
         }
-    
+
         else {
-            updateGame();
+           
         }
     }
 
-    $(".characters").on("click", function(){
+    $(".characters").on("click", function(){ //once a button is clicks the value does not change value if another button is clicked
+        
+        var value = $("button").attr("data-value");
+        totalScore = totalScore + parseInt(value);
+        console.log("Score: " + totalScore)
+        updateGame();
         winLose();
     });
 
