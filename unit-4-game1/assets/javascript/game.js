484f107ffb1e723badf459cb6bb947072ae969c6
nbrasaemle <nicole.brasaemle@gmail.com>
2018-06-05 13:44:48 -0500

    Unit 4 Game Complete

--- unit-4-game1/assets/javascript/game.js
+++ unit-4-game1/assets/javascript/game.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
+   
     var wins= 0;
     var losses = 0;
     var totalScore = 0;
     var randomNumber;
 
     function resetGame(){
-        
 
         totalScore = 0;
         //generate a random number for the random number,
@@ -15,26 +15,24 @@ $(document).ready(function(){
 
         //generate a random number for each button, 
         //have to either have the number above the max or add one to get the range desired
-        var valueGandalf = Math.floor(Math.random() * (13-1) + 1);
+        var valueGandalf = Math.floor(Math.random() * (13-1) + 1);//max number (121) not included
         console.log("Gandalf's value: " + valueGandalf);
-        var valueHobbits = Math.floor(Math.random() * (13-1) + 1); // hobbits value is coming up as gandalfs value
+        var valueHobbits = Math.floor(Math.random() * (13-1) + 1); 
         console.log("Hobbits' value: " + valueHobbits);
         var valueAragorn = Math.floor(Math.random() * (13-1) + 1);
         console.log("Aragorn's value: " + valueAragorn);
-        var valueBoromir = Math.floor(Math.random() * (13-1) + 1);//come up as gandalf
+        var valueBoromir = Math.floor(Math.random() * (13-1) + 1);
         console.log("Boromir's value: " + valueBoromir);
         
         //give the buttons these values.val() or .attr()
-        $("button.gandalf").attr("data-value", valueGandalf);
+        $("button.gandalf").attr("data-value", valueGandalf); //assigning values to buttons
         $("button.hobbits").attr("data-value", valueHobbits);
         $("button.aragorn").attr("data-value", valueAragorn);
         $("button.boromir").attr("data-value", valueBoromir);
         //print the random number 
         $(".randomNumber").html("<h2> " + randomNumber + " </h2>");
         //print the total score
-        $(".totalScore").html("<h2>Total Score: </h2> <h2> " + totalScore + " </h2>");
-        $("#wins").html("Wins: ");
-        $("#losses").html("Losses: ");
+        $(".totalScore").html("<h2>Total Score: </h2> <h2> " + totalScore + " </h2>");  
     }
 
     resetGame();
@@ -44,47 +42,43 @@ $(document).ready(function(){
     }
 
     function winLose() {
+        //Winner:
         if (totalScore == randomNumber) {
             wins++;
-            
+            console.log("W: " + wins);
             $("#wins").html("Wins: " + wins );
             $("#win_lose").html("The Fellowship has been reunited!")
             
     
             resetGame();
         }
-    
+        //Loser:
         else if (totalScore > randomNumber) {
             losses++;
+            console.log("L: " + losses);
             $("#losses").html("Losses: " + losses);
             $("#win_lose").html("The Fellowship is lost!")
             
     
             resetGame();
         }
-
-        else {
-           
-        }
     }
-
-    $(".characters").on("click", function(event){ //once a button is clicks the value does not change value if another button is clicked
+    //click function
+    $(".characters").on("click", function(event){ 
         if (event.target.classList.contains("btn")){
-       var value = $(this).attr("data-value");
-       console.log(event)
-        totalScore = totalScore + parseInt(value);
-        console.log("Score: " + totalScore);
-        }
+            var value = $(this).attr("data-value");
+            console.log(event);
+            totalScore = totalScore + parseInt(value);
+        } //getting the value and the button to match
 
         if (event.target.tagName=="IMG") {
             var value = $(event.target).parent().attr("data-value");
-            console.log(event)
-             totalScore = totalScore + parseInt(value);
-             console.log("Score: " + totalScore);   
-        }
-        
-        updateGame();
+            console.log(event);
+            totalScore = totalScore + parseInt(value);   
+        }// getting the image within the button to pair with the value of the button
+
+        console.log("Score: " + totalScore);   
+        updateGame(); 
         winLose(); 
     });
-
 });
